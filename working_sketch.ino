6172391a19a01ef01d967b73971f4082b2a5f7c2
Olga <poleshchukolala@gmail.com>
2022-06-12 00:04:54 +0300

    comments

--- working_sketch.ino
+++ working_sketch.ino
@@ -1,42 +1,40 @@
-#include <Adafruit_NeoPixel.h> // подключаем библиотеку
-// KY-037
-const int LED_PIN = 6;             // указываем пин для подключения ленты
-const int NUMPIXELS = 60;  // указываем количество светодиодов в ленте
+#include <Adafruit_NeoPixel.h> 
+
+//Define variables:
+#define SENSOR_PIN  A4
+const int LED_PIN = 6;            
+const int NUMPIXELS = 75; 
 const int INIT_BRIGHTNESS = 255;
+int sensor_state;
 
-const int SENSOR_PIN = 2;
-int sensor_state = 0;
-
-// создаем объект strip с нужными характеристиками
+//Initialize pixel strip
 Adafruit_NeoPixel strip (NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-void setup() {
-//  Serial.begin (115200);      
-  pinMode(SENSOR_PIN, INPUT);
-  strip.begin();                     // инициализируем ленту
-  strip.setBrightness(INIT_BRIGHTNESS);  // указываем яркость светодиодов (максимум 255)    
-  strip.show();
-}
-
+//Custom function, triggered when someone step on platform
 void react(int init_brightness=INIT_BRIGHTNESS) {
   int reaction_brightness = init_brightness;
   strip.setBrightness(reaction_brightness);
-  strip.fill(strip.Color(141, 90, 151), 0, NUMPIXELS);   // включаем белый цвет на 3 светодиоде
-  strip.show();   // отправляем сигнал на ленту
+  strip.fill(strip.Color(141, 90, 151), 0, NUMPIXELS);
+  strip.show();
     
-    // delay(500);
   while (reaction_brightness > 0){
     strip.setBrightness(reaction_brightness);
     strip.show();
     reaction_brightness -= 5;
     delay(50);
-    // sensor_state = digitalRead(SENSOR_PIN);
-    // if (sensor_state == 1){
-    //   break;
-    // }
     }
 }
 
+//setup function (required for Arduino)
+void setup() {
+  pinMode(SENSOR_PIN, INPUT);
+  strip.begin(); 
+  strip.setBrightness(INIT_BRIGHTNESS);  
+  strip.show();
+}
+
+
+//Loop function: required for Arduino, main logic defined here:
 void loop(){
   strip.clear(); 
   strip.setBrightness(INIT_BRIGHTNESS);
@@ -44,23 +42,22 @@ void loop(){
   
   int loop_brightness = INIT_BRIGHTNESS;
   sensor_state = digitalRead(SENSOR_PIN);
-  if (sensor_state == 0) {
+  if (sensor_state == 0 ) {
     strip.setPixelColor(random(NUMPIXELS),strip.Color(65, 211, 189));
     strip.show();
     while (loop_brightness > 0){
       sensor_state = digitalRead(SENSOR_PIN);
-      if (sensor_state == 1){
-        break;//react();
+      if (sensor_state > 0){
+        break;
       }
       strip.setBrightness(loop_brightness);
       strip.show();
       loop_brightness = loop_brightness - 10;
-      delay(50);
+      delay(30);
       }
-    if (sensor_state == 1){
+    if (sensor_state > 0){
         react();
       }
-    
   }
   else { 
     react();
